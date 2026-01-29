094b512d3383e120bad8678478ae65ffbd9649be
ifreddyman2014-tech <ifreddyman2014@gmail.com>
2026-01-29 21:27:11 +0700

    Create config.py file with configuration constants for MT5

---
+++ config.py
@@ -0,0 +1,20 @@
+# Configuration constants for MT5
+
+# MT5 Login credentials
+MT5_LOGIN = '<your_login>'
+MT5_PASSWORD = '<your_password>'
+MT5_SERVER = '<your_server>'
+
+# Timezones
+TIMEZONE_UTC = 'UTC'
+TIMEZONE_EST = 'America/New_York'
+
+# Periods
+PERIOD_M1 = 'M1'
+PERIOD_M5 = 'M5'
+PERIOD_H1 = 'H1'
+
+# Strategy Parameters
+STRATEGY_RISK = 0.01
+STRATEGY_TAKE_PROFIT = 100
+STRATEGY_STOP_LOSS = 50
